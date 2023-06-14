a49183a2e60116c50aa6cf89533a2663d6a05aa9
arcayi <arcayi@qq.com>
2023-06-14 15:42:01 +0800

    my env

--- docker/run.bash
+++ docker/run.bash
@@ -13,10 +13,14 @@ then
 fi
 
 docker run --net=host --rm -it \
+        -v=/data/Git_Repository/:/Git_Repository:rw \
         -v=/tmp/.X11-unix:/tmp/.X11-unix:rw \
         -v=${XAUTH}:${XAUTH}:rw \
         -e="XAUTHORITY=${XAUTH}" \
         -e="DISPLAY=${DISPLAY}" \
         -e=TERM=xterm-256color \
         -e=QT_X11_NO_MITSHM=1 \
+        -e=QT_X11_NO_MITSHM=1 \
+        -e=QT_AUTO_SCREEN_SCALE_FACTOR=1 \
+        -e=QT_SCALE_FACTOR=1.5 \
         onnxgraphqt:latest
